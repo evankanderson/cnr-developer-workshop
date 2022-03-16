ba2ee6e5b00f572fe1bd1e9f46f5d6fee9ee1f8c
Evan Anderson <evana@vmware.com>
2022-03-16 11:05:54 -0700

    Update setup to use workshop-created registry

--- workshop/setup.d/fetch-kn.sh
+++ workshop/setup.d/fetch-kn.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # This script assumes Docker Desktop is already installed on the user's machine.
 # It *does not* assume any specific docker credentials are loaded, and instead uses
@@ -10,12 +10,16 @@ FUNC_VARIANT="$(echo $VARIANT | tr '-' '_')"
 mkdir -p $HOME/.bin
 cd $HOME/.bin
 
-echo "Downloading kn and kn-plugin-func for $VARIANT"
+echo "Downloading kn and kn-plugin-func for $VARIANT" 1>&2
 
-curl -L -o kn https://github.com/knative/client/releases/download/knative-v1.2.0/kn-$VARIANT >&/dev/null
-curl -L -o kn-plugin-func https://github.com/knative-sandbox/kn-plugin-func/releases/download/v0.22.0/func_$FUNC_VARIANT >&/dev/null
+curl -L -o kn "https://github.com/knative/client/releases/download/knative-v1.2.0/kn-$VARIANT" >&/dev/null
+curl -L -o kn-plugin-func "https://github.com/knative-sandbox/kn-plugin-func/releases/download/v0.22.0/func_$FUNC_VARIANT" >&/dev/null
 
 chmod +x kn kn-plugin-func
 ln -s kn-plugin-func func
 
-echo "Installed plugins"
+echo "Installed plugins" 1>&2
+
+echo 'export PATH="${PATH}:${HOME}/.bin"' >> ${HOME}/.profile
+
+echo ". ${HOME}/.profile"
